ec06df0672f30127353d32f33c417f9610e5dca3
Sam.G <samuelgerungan@gmail.com>
2024-03-08 14:46:31 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import Image from "next/image";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <header>
+        <div className="container mx-auto flex flex-wrap p5 flex-col md:flex-row items-center">
+          <a className="flex title-font">
+            <h1>Sam Gerungan</h1>
+          </a>
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Home;
