811d7e5348f4bf5e01d48da6590b1960645c7cf3
Sam.G <samuelgerungan@gmail.com>
2024-03-08 18:20:45 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,11 +28,11 @@ const Home = () => {
       <main className="text-slate-100 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <p className="hidden lg:block text-xl my-1">Software Engineer</p>
             <h1 className="title-font sm:text-4xl text-5xl mb-4 font-medium">
               Samuel Gerungan
-              <p className="hidden lg:block text-xl my-1">Software Engineer</p>
             </h1>
-            <p className="my-5 leading-relaxed ml-1">
+            <p className="my-1 leading-relaxed ml-1">
               An eye for design, passion for innovation, and meticulous
               attention to detail
             </p>
