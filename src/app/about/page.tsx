9b04c6f8dc92b7cd7f2407a05c4712e4f5b279bf
Sam.G <samuelgerungan@gmail.com>
2024-03-10 00:12:52 -0800

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,28 +24,6 @@ const About = () => {
           </div>
         </div>
       </main>
-
-      <footer className="text-slate-200 body-font">
-        <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-          <a className="flex title-font font-medium items-center md:justify-start justify-center text-slate-100">
-            <span className="ml-3 text-xl">Sam Gerungan</span>
-          </a>
-          <p className="text-sm text-slate-300 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-            © 2024 Samuel Gerungan —
-            <a
-              href="https://https://github.com/Sam-T-G"
-              className="text-cyan-200 ml-1"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              @sam-t-g
-            </a>
-          </p>
-          <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-            {/* Social media icons */}
-          </span>
-        </div>
-      </footer>
     </>
   );
 };
