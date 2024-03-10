052bb9f25dab97b9ac8ba08be6a3aa49be070d84
Sam.G <samuelgerungan@gmail.com>
2024-03-10 00:37:25 -0800

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,11 +8,28 @@ const About = () => {
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <p className="hidden lg:block text-xl my-1">About Me</p>
             <h1 className="title-font sm:text-4xl text-5xl mb-4 font-medium">
-              About Sam Gerungan
+              Samuel Timothy Gerungan
             </h1>
             <p className="my-1 leading-relaxed ml-1">
-              Insert your About content here. You can describe yourself, your
-              skills, experiences, etc.
+              blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah
+            </p>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img
+              className="object-contain object-center rounded-3xl"
+              alt="hero-image"
+              src="/your-image.jpg"
+            />
+          </div>
+        </div>
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <p className="hidden lg:block text-xl my-1">About Me</p>
+            <h1 className="title-font sm:text-4xl text-5xl mb-4 font-medium">
+              Samuel Timothy Gerungan
+            </h1>
+            <p className="my-1 leading-relaxed ml-1">
+              blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah
             </p>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
