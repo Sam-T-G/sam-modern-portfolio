303aa14cccfd5d6eaa3afe697c314a3ce0b77ab4
Sam.G <samuelgerungan@gmail.com>
2024-03-10 00:03:30 -0800

    redundant code

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,39 +3,6 @@ import React from "react";
 const About = () => {
   return (
     <>
-      <header className="text-slate-100 body-font">
-        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a className="flex font-medium items-center mb-4 md:mb-0">
-            <h1 className="ml-3 text-2xl hover:text-cyan-500 cursor-pointer">
-              Sam Gerungan
-            </h1>
-          </a>
-          <nav className="md:ml-auto flex flex-wrap items-center justify-center">
-            <a
-              className="flex mr-5 hover:text-cyan-500 cursor-pointer"
-              href="/"
-            >
-              Home
-            </a>
-            <a
-              className="flex mr-5 hover:text-cyan-500 cursor-pointer"
-              href="/about"
-            >
-              About
-            </a>
-            <a
-              className="flex mr-5 hover:text-cyan-500 cursor-pointer"
-              href="/contact"
-            >
-              Contact
-            </a>
-          </nav>
-          <button className="inline-flex items-center bg-gray-100 text-cyan-800 border-0 py-1 px-3 focus:outline-none hover:bg-cyan-500 hover:text-white rounded text-base mt-4 md:mt-0">
-            Projects
-          </button>
-        </div>
-      </header>
-
       <main className="text-slate-100 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
