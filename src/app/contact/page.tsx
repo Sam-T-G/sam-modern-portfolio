22bba788e38ecad05b0893b0fcd750463620d822
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-11 15:00:14 -0700

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -27,7 +27,6 @@ const ContactCard = () => {
 								d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
 							/>
 						</svg>
-
 						<span className="text-gray-600"> Los Angeles, California</span>
 					</li>
 					<li className="flex items-center py-2">
@@ -44,21 +43,20 @@ const ContactCard = () => {
 								d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
 							/>
 						</svg>
-
 						<span className="text-gray-600">samuelgerungan@gmail.com</span>
 					</li>
 					<li className="flex items-center py-2">
 						<svg
-							className="h-6 w-6 text-gray-600 mr-3"
+							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
-							stroke="currentColor"
 							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg">
+							stroke-width="1.5"
+							stroke="currentColor"
+							className="w-6 h-6 text-gray-600 ml-1 mr-4">
 							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth={2}
-								d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
 							/>
 						</svg>
 						<span className="text-gray-600">+1(909)-810-6275</span>
