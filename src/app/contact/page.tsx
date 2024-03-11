ccafb37b3974f216032b04a12dea2fe6ff830388
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2024-03-11 14:57:57 -0700

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -15,7 +15,7 @@ const ContactCard = () => {
 							viewBox="0 0 24 24"
 							stroke-width="1.5"
 							stroke="currentColor"
-							className="w-6 h-6 text-gray-600 mr-4">
+							className="w-6 h-6 text-gray-600 ml-1 mr-4">
 							<path
 								stroke-linecap="round"
 								stroke-linejoin="round"
@@ -32,18 +32,19 @@ const ContactCard = () => {
 					</li>
 					<li className="flex items-center py-2">
 						<svg
-							className="h-6 w-6 text-gray-600 mr-3"
+							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
-							stroke="currentColor"
 							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg">
+							stroke-width="1.5"
+							stroke="currentColor"
+							className="w-6 h-6 text-gray-600 ml-1 mr-4">
 							<path
-								strokeLinecap="round"
-								strokeLinejoin="round"
-								strokeWidth={2}
-								d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
 							/>
 						</svg>
+
 						<span className="text-gray-600">samuelgerungan@gmail.com</span>
 					</li>
 					<li className="flex items-center py-2">
