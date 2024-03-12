05a172492ee7c0edb53b66890e28fc6596528f65
Sam.G <samuelgerungan@gmail.com>
2024-03-11 17:59:23 -0700

    Styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<footer className="text-slate-200 bg-neutral-900 body-font">
-			<div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+		<footer className="text-slate-200 body-font">
+			<div className="container px-5 py-8 mx-auto flex flex-col justify-between items-center sm:flex-row">
 				<a className="flex title-font font-medium items-center md:justify-start justify-center text-slate-100">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
 						fill="none"
 						stroke="currentColor"
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
 						className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
 						viewBox="0 0 24 24">
 						<path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -32,9 +32,9 @@ const Footer = () => {
 					<a className="text-gray-500">
 						<svg
 							fill="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
 							className="w-5 h-5"
 							viewBox="0 0 24 24">
 							<path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
@@ -43,9 +43,9 @@ const Footer = () => {
 					<a className="ml-3 text-gray-500">
 						<svg
 							fill="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
 							className="w-5 h-5"
 							viewBox="0 0 24 24">
 							<path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
@@ -55,9 +55,9 @@ const Footer = () => {
 						<svg
 							fill="none"
 							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
 							className="w-5 h-5"
 							viewBox="0 0 24 24">
 							<rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
@@ -68,9 +68,9 @@ const Footer = () => {
 						<svg
 							fill="currentColor"
 							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="0"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="0"
 							className="w-5 h-5"
 							viewBox="0 0 24 24">
 							<path
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navbar = () => {
 	return (
-		<header className="text-slate-100 bg-neutral-900 body-font">
+		<header className="text-slate-100 body-font">
 			<div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
 				<a className="flex font-medium items-center mb-4 md:mb-0">
 					<h1 className="ml-3 text-2xl hover:text-cyan-500 cursor-pointer">
