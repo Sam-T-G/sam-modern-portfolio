0e1e5d9a5f5244d8fe90f3c9016602add0fe3421
Sam.G <samuelgerungan@gmail.com>
2024-03-12 14:58:46 -0700

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -83,7 +83,7 @@ const ContactCard = () => {
 									d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
 								/>
 							</svg>
-							<span className="text-gray-600"> Los Angeles, California</span>
+							<span className="text-gray-600">GitHub</span>
 						</li>
 						<li className="flex items-center py-2">
 							<svg
@@ -99,7 +99,7 @@ const ContactCard = () => {
 									d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
 								/>
 							</svg>
-							<span className="text-gray-600">samuelgerungan@gmail.com</span>
+							<span className="text-gray-600">LinkedIn</span>
 						</li>
 						<li className="flex items-center py-2">
 							<svg
@@ -115,7 +115,7 @@ const ContactCard = () => {
 									d="M10.5 1.5H8.25A2.25 2.25 0 0 0 6 3.75v16.5a2.25 2.25 0 0 0 2.25 2.25h7.5A2.25 2.25 0 0 0 18 20.25V3.75a2.25 2.25 0 0 0-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3"
 								/>
 							</svg>
-							<span className="text-gray-600">+1(909)-810-6275</span>
+							<span className="text-gray-600">Instagram</span>
 						</li>
 					</div>
 				</ul>
