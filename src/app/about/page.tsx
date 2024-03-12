481a0f1a903ecf3263bf159c0823e0117b2bb8af
Sam.G <samuelgerungan@gmail.com>
2024-03-12 13:39:04 -0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -28,8 +28,9 @@ const features = [
 ];
 
 const images = [
-	"https://images.unsplash.com/photo-1485433592409-9018e83a1f0d?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-	"https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+	"../public/bali-rice-terrace.jpg",
+	"../public/culver-modern.jpg",
+	"../public/bali-sunset.jpg",
 ];
 
 const children = [];
@@ -99,7 +100,8 @@ const About: React.FC = () => {
 							<ImagesSlider
 								className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
 								images={images}>
-								{/* Empty children */}
+								children={null}
+								overlay={false}
 							</ImagesSlider>
 						</div>
 					</div>
