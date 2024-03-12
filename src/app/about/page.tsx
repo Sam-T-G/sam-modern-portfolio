4eefe6596f1a3b372be8b528108faa70427aa6b6
Sam.G <samuelgerungan@gmail.com>
2024-03-12 13:44:15 -0700

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,7 @@ import {
 	GlobeAltIcon,
 	ViewfinderCircleIcon,
 } from "@heroicons/react/20/solid";
+import Image from "next/image";
 import { ImagesSlider } from "@/components/images-slider";
 
 const features = [
@@ -90,19 +91,19 @@ const About: React.FC = () => {
 							</div>
 						</div>
 						<div>
-							{/* <img
-								src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+							<Image
+								src="/culver-modern.jpg"
 								alt="Product screenshot"
 								className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
 								width={2432}
 								height={1442}
-							/> */}
-							<ImagesSlider
+							/>
+							{/* <ImagesSlider
 								className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
 								images={images}>
 								children={null}
 								overlay={false}
-							</ImagesSlider>
+							</ImagesSlider> */}
 						</div>
 					</div>
 				</div>
