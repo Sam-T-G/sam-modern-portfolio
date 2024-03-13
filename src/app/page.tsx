ef92d7993f944f48540f97e0aeb5339813b322f8
Sam.G <samuelgerungan@gmail.com>
2024-03-12 22:35:53 -0700

    Motion implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
@@ -7,7 +8,7 @@ const Home = () => {
 		<>
 			<main className="text-gray-800 bg-white body-font">
 				<div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-					<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+					<motion.div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
 						<p className="hidden lg:block font-semibold text-xl text-indigo-600 my-1 tracking-wide">
 							Software Engineer
 						</p>
@@ -18,7 +19,7 @@ const Home = () => {
 							An eye for design, passion for innovation, and meticulous
 							attention to detail
 						</p>
-					</div>
+					</motion.div>
 					<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
 						<Image
 							className="object-contain object-center rounded-3xl"
